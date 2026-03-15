c6511759cfe0362ad68e465f6e92b35fcdb26158
Victorallvin27 <allvinvictor.com@gmail.com>
2026-03-15 19:48:17 +0530

    latest code

--- apps/order-service/src/app.module.ts
+++ apps/order-service/src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { OrderModule } from './order/order.module';
-import { AdminAuthModule } from 'apps/product-service/src/admin-auth/admin-auth.module';
+// import { AdminAuthModule } from 'apps/product-service/src/admin-auth/admin-auth.module';
 import { SellerModule } from './seller/seller.module';
 import { SellerAuthModule } from './seller-auth/seller-auth.module';
-// import { AuthModule } from '../src/common/auth/auth.module';
-import {AuthModule} from 'apps/product-service/src/auth/auth.module';
+import { AuthModule } from '../src/common/auth/auth.module';
+// import {AuthModule} from 'apps/product-service/src/auth/auth.module';
 
 @Module({
   imports: [
@@ -24,7 +24,7 @@ import {AuthModule} from 'apps/product-service/src/auth/auth.module';
       }),
     }),
 
-    // AuthModule,
+    AuthModule,
     OrderModule,
     // AdminAuthModule,
     SellerModule,
---
+++ apps/order-service/src/common/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from '../strategies/jwt.strategy'; 
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'user-jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'), // Same secret as product-service
+        signOptions: { expiresIn: '7d' },
+      }),
+    }),
+  ],
+  providers: [JwtStrategy],
+  exports: [JwtStrategy, PassportModule, JwtModule],
+})
+export class AuthModule {}
